3b35099debea3190bad365150812104383c8f0c8
Mohamad Usama KUNNATHUR <mohamadusama242@gmail.com>
2018-07-11 19:41:12 +0200

    modified the restart function to remove classes show and open

--- js/app.js
+++ js/app.js
@@ -1,16 +1,5 @@
-/*
- * Create a list that holds all of your cards
- */
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length,
         temporaryValue, randomIndex;
@@ -25,32 +14,19 @@ function shuffle(array) {
 
     return array;
 }
-
+const cards = $('.card');
 // This function adds as an event listener listening to whenever a user presses restart it shuffles the cards
 $('.restart').bind('click', function () {
-    const shuffled = shuffle($('.card'))
+	for (let i = 0; i<cards.length; i++) {
+		cards[i].classList.remove('show', 'open');
+	};
+    const shuffled = shuffle(cards)
     $('.deck').append(shuffled);;
 });
 
 
-const cards = $('.card');
+//This turns the cards
 cards.on('click',function(e) {
     e.target.classList.toggle('open');
 	e.target.classList.toggle('show');
 })
-// for (i=0; i<=cards.length; i++) {
-//     cards[i].on('click', function() {
-//     console.log(cards[i]);
-//     })
-// }
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
