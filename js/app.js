c6ffe53f1bc6ba46f4a7714618ce7c97db4cff3a
Mohamad Usama KUNNATHUR <mohamadusama242@gmail.com>
2018-07-11 20:34:06 +0200

    Added function to chec if cards match

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-
+//The global variables
+const cards = $('.card');
+let matched = $('.match');
+let openCards = [];
 
 function shuffle(array) {
     var currentIndex = array.length,
@@ -11,10 +14,10 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
 }
-const cards = $('.card');
+
+
 // This function adds as an event listener listening to whenever a user presses restart it shuffles the cards
 $('.restart').bind('click', function () {
 	for (let i = 0; i<cards.length; i++) {
@@ -24,9 +27,33 @@ $('.restart').bind('click', function () {
     $('.deck').append(shuffled);;
 });
 
+//This function checks if the opened cards match
+function isMatch() {
+	openCards.push(this);
+    if(openCards.length === 2){
+        if(openCards[0].type === openCards[1].type){
+            openCards[0].classList.add("match", "disabled");
+            openCards[1].classList.add("match", "disabled");
+            openCards[0].classList.remove("show", "open", "no-event");
+            openCards[1].classList.remove("show", "open", "no-event");
+        } else {
+            openCards[0].classList.add("unmatched");
+            openCards[1].classList.add("unmatched");
 
-//This turns the cards
+        setTimeout(function(){
+            openCards[0].classList.remove("show", "open", "no-event","unmatched");
+            openCards[1].classList.remove("show", "open", "no-event","unmatched");
+            },1000);
+        }
+		openCards = [];
+    }
+};
+		
+
+//This turns the cards uses event delegation
 cards.on('click',function(e) {
     e.target.classList.toggle('open');
 	e.target.classList.toggle('show');
+	e.target.classList.toggle('disabled');
+	isMatch();
 })
