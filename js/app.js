695777b77ddea0651cbd8fba7e5d22e57d392f48
Mohamad Usama KUNNATHUR <mohamadusama242@gmail.com>
2018-07-12 17:18:58 +0200

    Organized the code

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//------------------------VARIABLES----------------------------
+
 //Time variables
 const timer = document.getElementById('timer');
 let seconds = 0;
@@ -9,6 +11,7 @@ let timerStart;
 const cards = $('.card');
 let matched = $('.match');
 
+//------------------------FUNCTIONS----------------------------
 
 function shuffle(array) {
     var currentIndex = array.length,
@@ -24,15 +27,14 @@ function shuffle(array) {
     return array;
 }
 
-
-// This function adds as an event listener listening to whenever a user presses restart it shuffles the cards
-$('.restart').bind('click', function () {
+function restart() {
 	cards.map(x => {
 	cards[x].classList.remove('show', 'open', 'disabled', 'match');
 	});
     const shuffled = shuffle(cards)
     $('.deck').append(shuffled);;
-});
+};
+	
 
 //This function checks if the opened cards match
 function isMatch() {
@@ -41,7 +43,7 @@ function isMatch() {
         if(openCards[0].innerHTML === openCards[1].innerHTML){
 			openCards.map(x => {
 			openCards[x].classList.remove("open", "show")
-			openCards[x].classList.add("match", 'disabled');
+			openCards[x].classList.add("match");
             });
         } else {
             setTimeout(() => openCards.map(x => {
@@ -52,11 +54,23 @@ function isMatch() {
 };
 		
 	let openCards = $('.open');
+
+
+//------------------------EVENT LISTENERS----------------------------
+	
+// This function adds as an event listener listening to whenever a user presses restart it shuffles the cards
+$('.restart').bind('click', restart)
 	
 //This turns the cards using event delegation to assign event listener
 cards.on('click',function(e) {
-	e.target.classList.toggle('open');
-	e.target.classList.toggle('show');
-	e.target.classList.toggle('disabled');
+	let openCards = $('.open');
+	if(openCards.length < 2){
+		e.target.classList.toggle('open');
+		e.target.classList.toggle('show');
+		e.target.classList.toggle('disabled');
+	}
 	isMatch();
 });
+
+//------------------------------MAIN---------------------------------
+restart();
